8fd7fffabbb8af5402a7d42d523f751df9ca5db5
Pedro Baracho <pedropbaracho@gmail.com>
2017-01-15 17:28:33 -0200

    Implemented exercise

--- src/app/rollover-image.directive.ts
+++ src/app/rollover-image.directive.ts
@@ -1,9 +1,35 @@
-import { Directive } from '@angular/core';
+import { 
+  Directive,
+  Input,
+  HostBinding,
+  HostListener
+} from '@angular/core';
 
 @Directive({
   selector: 'img[ccRollover]'
 })
 export class RolloverImageDirective {
+
+  @Input('ccRollover') config = {
+    'initial':'https://unsplash.it/200/300?image=201',
+    'over':'https://unsplash.it/200/300?image=202'
+  }
+
+  @HostBinding('src') private imagePath: String;
+
+  ngOnChanges() {
+    if (this.config.initial) {
+      this.imagePath = this.config.initial;
+    }
+  }
+
+  @HostListener('mouseover') onMouseOver() {
+    this.imagePath = this.config.over;
+  }
+
+  @HostListener('mouseout') onMouseOut() {
+    this.imagePath = this.config.initial;
+  }
 
   constructor() { }
 
